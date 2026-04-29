25784807fdf6839cc54dad1ca2e4fec60a2a1399
LaMarcus White <mrlamarcuswhite@gmail.com>
2026-04-29 14:42:01 -0400

    Created HTML and JS documents. Completed Lab Instructions

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//Exercise 1
+
+console.log(2 * 3 + 1); //expected 7
+console.log(2 ** 4); //expected 16
+console.log(5 * 1); //expected 5
+console.log(8 ** 2 + 5 ** 2); //expected 39
+
+//Exercise 2
+console.log(4 * 5 === 20); //expected true
+console.log(6 * 5 !== "30"); //expected true
+console.log(-17 < 0); //expected true
+console.log(25 > 1); //expected true
+console.log(2 + 2 * 2 > 4); //expected true
+
+//Exercise 3
+console.log(true || false); //expected true
+console.log(false === false); //expected true
+console.log(false && false || true); //expected true
+console.log(true || false || false && true); //expected true
